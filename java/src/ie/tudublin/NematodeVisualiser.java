e08e060d5a5ad965721f84338ecf880675fa17de
StephenHughes <StephenHughesxx@outlook.com>
2022-04-27 16:59:06 +0100

    Load and Display finished

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -9,6 +9,8 @@ import processing.data.TableRow;
 public class NematodeVisualiser extends PApplet
 {
 
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
@@ -35,10 +37,19 @@ public class NematodeVisualiser extends PApplet
 		Table table = loadTable("nematodes.csv", "header");
 		for (TableRow r:table.rows())
 		{
-			
+			Nematode n = new Nematode(r);
+			nematodes.add(n);
 		}
 	}
 
+
+	public void displayNematode()
+	{
+		for (Nematode n:nematodes)
+		{
+			System.out.println(n);
+		}
+	}
 
 	public void draw()
 	{	
